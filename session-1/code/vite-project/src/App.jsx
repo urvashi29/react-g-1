ba025e9e1118133187005dde2393fe7a4020fbec
urvashi29 <urvashigolu29@gmail.com>
2026-05-14 01:06:58 +0530

    first commit

---
+++ session-1/code/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+// return JSX(html look like syntax)
+// Root component
+
+const App = () => {
+  return (
+    <>
+      <h2>Welcome!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate et
+        dignissimos, nulla labore maxime dicta animi nam aliquam assumenda
+        ducimus aperiam hic ipsam, amet ex aut ea atque, explicabo magnam.
+      </p>
+    </>
+  );
+};
+
+export default App;
+
+// render -> display
