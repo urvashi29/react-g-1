ac1413157e65a42d58c54d1fa90b0554e668b247
urvashi29 <urvashigolu29@gmail.com>
2026-06-04 22:21:33 +0530

    added errorboundary in session-7

--- session-7/code/src/App.jsx
+++ session-7/code/src/App.jsx
@@ -3,20 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Notes from "./components/Notes";
 import ProtectedRoute from "./components/ProtectedRoute";
+import ErrorBoundary from "./components/ErrorBoundary";
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/notes"
-          element={
-            <ProtectedRoute>
-              <Notes />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/notes"
+            element={
+              <ProtectedRoute>
+                <Notes />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
     </>
   );
 };
